bf03bdd2455363ee43ea34c393de822d6593eed3
osman <osman.ahmady@gmail.com>
2017-02-17 11:53:03 +0100

    Recuperé api OpenDataSoft, paramétrage des events: concerts, en ile de france(modifie)

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ $(document).ready(function() {
 
     var divContainer = $(".links");
 
+// le lien d'api
+
     $("#btn").on("click", function() {
         $.getJSON("https://public.opendatasoft.com/api/v2/catalog/datasets/evenements-publics-cibul/records?q=%20%22%C3%8Ele-de-France%22%2C%20%22Concert%22%2C%20%222017%22&rows=30&start=22&pretty=false&timezone=UTC", function(json) {
             // $(".links").html(JSON.stringify(json));
             console.log(json);
+
+       // afficher l'api
 
             json.records.forEach(function(item) {
 
@@ -21,18 +25,18 @@ $(document).ready(function() {
                 DatEnd = $("<p>Date de fin:</p>");
                 divContainer.append(DatEnd);
                 DatEnd.append(" " +item.record.fields.date_end);
-                adress = $("<p>Adresse:</p>");
-                divContainer.append(adress);
-                adress.append(" " +item.record.fields.address);
+                adresse = $("<p>Adresse:</p>");
+                divContainer.append(adresse);
+                adresse.append(" " +item.record.fields.address);
                 prix = $("<p>Le prix:</p>");
                 divContainer.append(prix);
                 prix.append(" " +item.record.fields.pricing_info);
                 creLink = $("<a>Le lien vers le site: </a>");
                 creHref = creLink.attr("href", item.record.fields.link)
                 divContainer.append(creLink);
-                explication = $("<p>Description:</p>");
-                divContainer.append(explication);
-                explication.append(" " +"<br>" +item.record.fields.description);
+                description = $("<p>Description:</p>");
+                divContainer.append(description);
+                description.append(" " +"<br>" +item.record.fields.description);
 
 
             });
